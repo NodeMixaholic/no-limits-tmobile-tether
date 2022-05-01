5d8bd082a89277f4503101d41d0c382db160f378
Samuel Lord <sam@sllord.info>
2022-05-01 16:50:03 -0500

    Update no-limits-tether-warp.sh

--- no-limits-tether-warp.sh
+++ no-limits-tether-warp.sh
@@ -1,5 +1,6 @@
 echo "Press enter once connected to hotspot."
 read
-echo Changing TTL...
+echo Changing TTL and connecting to warp...
 sudo iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
 warp-cli connect
+echo Done.
